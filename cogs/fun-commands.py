4f78b8b22b549f2a2678aa845b6ce214ac337e27
Nexite <nikhil@gargmail.com>
2020-03-25 13:57:34 -0700

    Added logging for crab rave

--- cogs/fun-commands.py
+++ cogs/fun-commands.py
@@ -11,6 +11,7 @@ class FunCommands(commands.Cog, name="Fun Commands"):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
         self.random_channel = int(getenv("CHANNEL_RANDOM", 689534362760642676))
+        self.mod_log = int(getenv("CHANNEL_MOD_LOG", 689216590297694211))
 
     @commands.command(aliases=['crabrave', 'crab_rave', 'crab-rave'], hidden=True)
     async def crab(self, ctx, *, text):
@@ -24,6 +25,7 @@ class FunCommands(commands.Cog, name="Fun Commands"):
                             return await ctx.send('Could not download file...')
                         data = io.BytesIO(await resp.read())
                         await ctx.send(file=discord.File(data, f'{text}.mp4'))
+            await self.bot.get_channel(self.mod_log).send(f"{ctx.author.mention} did crab rave with arguments '{text}'")
         else:
             await ctx.send("Sorry, please do that in #random")
 
