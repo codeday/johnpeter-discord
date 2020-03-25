7fc16e4867b14cbc485468362057bc6c1e0026c6
Jake <royalcows9@gmail.com>
2020-03-25 14:45:42 -0700

    Started adding cacheing

--- cogs/fun-commands.py
+++ cogs/fun-commands.py
@@ -1,37 +1,71 @@
 import io
-from os import getenv
+from os import getenv, path, makedirs
 
 import aiohttp
 import discord
 from discord.ext import commands
 from urllib import parse
+import json
+import typing
 
 
 class FunCommands(commands.Cog, name="Fun Commands"):
     def __init__(self, bot):
+        self.cache_folder = "./cacherave/"
+        self.cache_index = "./cacherave.json"
         self.bot: commands.Bot = bot
         self.random_channel = int(getenv("CHANNEL_RANDOM", 689534362760642676))
         self.mod_log = int(getenv("CHANNEL_MOD_LOG", 689216590297694211))
+        try:
+            with open(self.cache_index, "w+") as file:
+                self.cache_data = json.load(file)
+        except:
+            self.cache_data = {}
+            print("Cache data not found!")
 
     @commands.command(aliases=['crabrave', 'crab_rave', 'crab-rave'], hidden=True)
-    async def crab(self, ctx, *, text=None):
-        if text is None:
-            await ctx.send("Please add arguments!")
+    async def crab(self, ctx, *, text):
+        if ctx.channel.id == self.random_channel:
+            await ctx.message.delete()
+
+            async with ctx.channel.typing():
+                resp = await self.get_with_cache(text)
+                if not resp:
+                    return await ctx.send('Could not download file...')
+                await ctx.send(file=discord.File(resp, f'{text}.mp4'))
+            await self.bot.get_channel(self.mod_log).send(f"{ctx.author.mention} did crab rave with arguments '{text}'")
         else:
-            if ctx.channel.id == self.random_channel:
-                await ctx.message.delete()
-                url = f'https://adventurous-damselfly.glitch.me/video/{parse.quote(text)}.mp4?style=classic'
-                async with ctx.channel.typing():
-                    async with aiohttp.ClientSession() as session:
-                        async with session.get(url) as resp:
-                            if resp.status != 200:
-                                return await ctx.send('Could not download file...')
-                            data = io.BytesIO(await resp.read())
-                            await ctx.send(file=discord.File(data, f'{text}.mp4'))
-                await self.bot.get_channel(self.mod_log).send(
-                    f"{ctx.author.mention} did crab rave with arguments '{text}'")
-            else:
-                await ctx.send("Sorry, please do that in #random")
+            await ctx.send("Sorry, please do that in #random")
+
+        # save cache to disk here https://docs.python.org/3/library/functions.html#open w+
+
+    async def get_with_cache(self, string) -> typing.Union[io.BytesIO, bool]:
+        """
+            Will take a requests string and return a file from cache if it exists, or from the interent if not.
+            Also saves new files to disk, keeping last 1000
+        """
+        if not path.exists(self.cache_folder):
+            makedirs(self.cache_folder)
+
+        if string in self.cache_data:
+            print("Using from cache")
+            with open(self.cache_data[string]["file"], "rb") as output_file:
+                self.cache_data[string]["usages"] += 1
+                return io.BytesIO(output_file.read())
+        else:
+            print("Downloading...")
+            url = f'https://adventurous-damselfly.glitch.me/video/{parse.quote(string)}.mp4?style=classic'
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    if resp.status != 200:
+                        return False
+                    with open(f"{self.cache_folder}{string}.mp4", "bw+") as output_file:
+                        output_file.write(await resp.read())
+                        self.cache_data[string] = {
+                            "file": f"{self.cache_folder}{string}.mp4",
+                            "usages": self.cache_data[string]["usages"] + 1
+                        }
+                    return io.BytesIO(await resp.read())
 
 
 def setup(bot):
