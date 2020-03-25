670a891a30e9126b86be1f1a509b937336e51061
Nexite <nikhil@gargmail.com>
2020-03-25 14:03:23 -0700

    Added error message for no arguments

--- cogs/fun-commands.py
+++ cogs/fun-commands.py
@@ -14,20 +14,23 @@ class FunCommands(commands.Cog, name="Fun Commands"):
         self.mod_log = int(getenv("CHANNEL_MOD_LOG", 689216590297694211))
 
     @commands.command(aliases=['crabrave', 'crab_rave', 'crab-rave'], hidden=True)
-    async def crab(self, ctx, *, text):
-        if ctx.channel.id == self.random_channel:
-            await ctx.message.delete()
-            url = f'https://adventurous-damselfly.glitch.me/video/{parse.quote(text)}.mp4?style=classic'
-            async with ctx.channel.typing():
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(url) as resp:
-                        if resp.status != 200:
-                            return await ctx.send('Could not download file...')
-                        data = io.BytesIO(await resp.read())
-                        await ctx.send(file=discord.File(data, f'{text}.mp4'))
-            await self.bot.get_channel(self.mod_log).send(f"{ctx.author.mention} did crab rave with arguments '{text}'")
+    async def crab(self, ctx, *, text = None):
+        if text == None:
+            await ctx.send("Please add arguments!")
         else:
-            await ctx.send("Sorry, please do that in #random")
+            if ctx.channel.id == self.random_channel:
+                await ctx.message.delete()
+                url = f'https://adventurous-damselfly.glitch.me/video/{parse.quote(text)}.mp4?style=classic'
+                async with ctx.channel.typing():
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(url) as resp:
+                            if resp.status != 200:
+                                return await ctx.send('Could not download file...')
+                            data = io.BytesIO(await resp.read())
+                            await ctx.send(file=discord.File(data, f'{text}.mp4'))
+                await self.bot.get_channel(self.mod_log).send(f"{ctx.author.mention} did crab rave with arguments '{text}'")
+            else:
+                await ctx.send("Sorry, please do that in #random")
 
 
 def setup(bot):
