bc2a789609470a00e381c6625e17f77a86a0b6bc
Nexite <nikhil@gargmail.com>
2020-03-25 18:54:05 -0700

    fixed merge issues

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ logging.basicConfig(level=logging.INFO)
 client = firestore.Client()
 collection = client.collection('teams')
 
-initial_cogs = ['cogs.team-builder', 'cogs.cleverbot', 'cogs.admin-commands', 'cogs.tournament']
+initial_cogs = ['cogs.team-builder', 'cogs.cleverbot', 'cogs.admin-commands', 'cogs.tournament', 'cogs.fun-commands']
 
 # Here we load our extensions(cogs) listed above in [initial_extensions].
 for cog in initial_cogs:
