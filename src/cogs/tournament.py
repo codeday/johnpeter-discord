dfb1aaf32559f18fa2ee57863dab39d76965761d
Zeke Egherman <zeke@egherman.com>
2020-03-30 18:12:54 -0500

    refactor essential commands to use new data structure

--- src/cogs/tournament.py
+++ src/cogs/tournament.py
@@ -1,16 +1,15 @@
-import asyncio
 import logging
-import random
 
 import discord
 from discord.ext import commands
 
-from utils.groups import make_groups
+from database.tournaments import Tournament
+from utils.person import id_from_mention
 
 
 class TournamentCog(commands.Cog, name="Tournament Helper"):
     """Creates Tournament Brackets!"""
-
+    # TODO: Make it support running multiple tournaments at the same time
     def __init__(self, bot):
         self.bot: commands.Bot = bot
         self.category = int(692803392031948911)  # gaming tournament
@@ -24,211 +23,122 @@ class TournamentCog(commands.Cog, name="Tournament Helper"):
 
     @tournament.command(name="create")
     @commands.has_any_role('Tournament Master')
-    async def tourney_create(self, ctx: commands.context.Context, game_name: str, emoji=':trophy:'):
-        """Creates a tournament with the given name and reaction emoji."""
+    async def tourney_create(self, ctx: commands.context.Context, game_name: str):
+        """Creates a tournament with the given name."""
         await ctx.message.delete()
-        self.gamers = []
-        self.enabled = True
-        self.round = -1
         logging.debug("Starting tournament creation...")
-        # Creates a new channel for the tournament
-        self.game_name = game_name
-        self.emoji = emoji
-        self.overwrites = {
-            ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False),
-            ctx.guild.get_role(self.role_student): discord.PermissionOverwrite(send_messages=False),
-            ctx.guild.me: discord.PermissionOverwrite(read_messages=True)
-        }
-        self.tc = ctx.channel
-        # Creates and sends the join message
-        self.join_message: discord.Message = await ctx.channel.send(
-            make_join_message(game_name, emoji, self.gamers)
-        )
-        await self.join_message.add_reaction('🏆')
+        t = Tournament(game_name=game_name,
+                       tc_id=ctx.channel.id,
+                       join_message_id=await ctx.channel.send(Tournament.make_join_message(game_name)).id
+                       )
+        self.tournaments.append(t)
+        await t.join_message(self.bot).add_reaction('🏆')
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        if payload.event_type == "REACTION_ADD" and payload.emoji.name == '🏆' and payload.message_id == self.join_message.id and self.enabled is True:
-            if payload.user_id != self.bot.user.id:  # John must not become gamer
-                self.gamers.append(payload.user_id)
-            await self.join_message.edit(content=make_join_message(self.game_name, self.emoji, self.gamers))
+        if (
+                payload.event_type == 'REACTION_ADD'
+                and payload.emoji.name == '🏆'
+                and any(payload.message_id == t.join_message_id for t in self.tournaments)
+                and payload.user_id != self.bot.user.id
+        ):
+            t = next((t for t in self.tournaments if t.join_message_id == payload.message_id))
+            if not t.add_gamer(payload.user_id):
+                user = self.bot.get_user(payload.user_id)
+                while user.dm_channel is None:
+                    await user.create_dm()
+                await user.dm_channel.send(
+                    'Sorry, but the tournament is already running, I am unable to add you'
+                )
+            await t.join_message(self.bot).edit(content=t.update_join_message())
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
-        if payload.event_type == "REACTION_REMOVE" and payload.emoji.name == '🏆' and payload.message_id == self.join_message.id and self.enabled is True:
-            self.gamers.remove(payload.user_id)
-            await self.join_message.edit(content=make_join_message(self.game_name, self.emoji, self.gamers))
+        if (
+                payload.event_type == 'REACTION_REMOVE'
+                and payload.emoji.name == '🏆'
+                and any(payload.message_id == t.join_message_id for t in self.tournaments)
+                and payload.user_id != self.bot.user.id
+        ):
+            t = next((t for t in self.tournaments if t.join_message_id == payload.message_id))
+            if not t.remove_gamer(payload.user_id):
+                user = self.bot.get_user(payload.user_id)
+                while user.dm_channel is None:
+                    await user.create_dm()
+                await user.dm_channel.send(
+                    'Sorry, but the tournament has already started, so I am unable to remove you.\
+                     If you have to leave, please inform the @Tournament Master'
+                )
+            await t.join_message(self.bot).edit(content=t.update_join_message())
 
     @tournament.command(name="round")
     @commands.has_any_role('Tournament Master')
-    async def tourney_round(self, ctx: commands.context.Context, groupSize=4):
-        """Creates a round of matches of the specified size."""
-        random.shuffle(self.gamers)
-        self.round += 1
-        await ctx.message.delete()
-        self.enabled = False
-        groups = make_groups(self.gamers, groupSize)
-        self.games = await create_games(groups=groups, ctx=ctx, game_name=self.game_name, overwrites=self.overwrites,
-                                        category=self.category)
-        for game in self.games:
-            for gamer in self.games[game]['gamers']:
-                if gamer is not None:
-                    await self.games[game]['tc'].set_permissions(ctx.guild.get_member(gamer),
-                                                                 read_messages=True,
-                                                                 manage_messages=True)
-                    await self.games[game]['vc'].set_permissions(ctx.guild.get_member(gamer),
-                                                                 read_messages=True,
-                                                                 send_messages=True)
-
-            await ctx.guild.get_channel(self.games[game]['tc'].id).send(
-                f'''Cowabunga, Gamers! :cowboy:
-Welcome to the Game Tournament! Please join the associated voice channel. It is now time to fight your fellow comrades. When you are finished, please use the command ~winner with who won.
-Game on! {''.join([f'<@{gamer}> ' for gamer in self.games[game]['gamers'] if gamer != None])}'''
-            )
-            await ctx.guild.get_channel(self.games[game]['tc'].id).send(
-                f'''<@{random.choice(self.games[game]['gamers'])}> has been randomly selected as the game host. Please send them a link to your steam profile so y'all can begin the HIGH OCTANE GAMING ACTION! :race_car:'''
-            )
-            await self.tc.send(f'Round {self.round} has started!')
-        self.join_message = await self.tc.send(make_running_message(self.game_name, self.games, self.round))
-        self.gamers = []
+    async def tourney_round(self, ctx: commands.context.Context, idx=None):
+        """Creates the next round"""
+        t = self.tournaments[0]
+        if t.next_round(bot=self.bot):
+            await ctx.message.delete()
+            for game in t.rounds[-1].games:
+                game.create_channel(ctx=ctx, game_name=t.game_name, category=self.category)
+        else:
+            await ctx.send('Previous round not yet finished! Aborting')
 
     @tournament.command(name="winner-set", aliases=["winner_set", "round-winner-set", "round_winner_set"])
     @commands.has_any_role('Tournament Master')
     async def round_winner_set(self, ctx: commands.context.Context, winner):
         """Sets the winner of a round."""
-        winner_id = int(winner.replace('<', '').replace('!', '').replace('>', '').replace('@', ''))
-        if winner_id in self.games[ctx.channel.id]['gamers']:
-            self.gamers.append(winner_id)
-            self.games[ctx.channel.id]['winner'] = winner_id
-            await self.join_message.edit(content=make_running_message(self.game_name, self.games, self.round))
-            if len(self.games) == 1:
-                await self.tc.send(
-                    f'<@{winner_id}> has won the tournament! Congratulations!~~')
-            else:
-                await self.tc.send(
-                    f'Congratulations to <@{winner_id}> for winning round {self.round} game {self.games[ctx.channel.id]["idx"]}!')
-            await asyncio.sleep(5)
-            await self.games[ctx.channel.id]['tc'].delete()
-            await self.games[ctx.channel.id]['vc'].delete()
+        t = self.tournaments[0]
+        winner_id = id_from_mention(winner)
+        game = t.latest_round.game_from_channel_id(ctx.channel.id)
+        if winner_id in t.latest_round.game_from_channel_id(ctx.channel.id).gamers:
+            game.set_winner(winner_id, self.bot)
 
     @tournament.command(name="winner",
                         aliases=["round-winner", "round_winner", 'votewinner', 'vote_winner', 'vote-winner',
                                  'roundwinner'])
     async def round_winner(self, ctx: commands.context.Context, winner=None):
         """Votes for the winner of a round."""
-        if ctx.channel.id in self.games:
-            game = self.games[ctx.channel.id]
-        else:
+        t = self.tournaments[0]
+        winner_id = id_from_mention(winner)
+        game = t.latest_round.game_from_channel_id(ctx.channel.id)
+        if game and winner_id:
+            game.vote(ctx.author.id, winner, self.bot)
+        elif not game:
             await ctx.channel.send(
-                '''I'm sorry, but this is not a known channel for a round. Please retry your command in a channel for a tournament match.''')
-            return
-        try:
-            winner_id = int(winner.replace('<', '').replace('!', '').replace('>', '').replace('@', ''))
-        except:
-            await ctx.channel.send('''I'm sorry, I don't know who you're talking about! Please use the command as follows, mentioning the person who won:
-            ~winner <@689549152275005513>''')
-            return
-        if winner_id in game['gamers']:
-            self.games[ctx.channel.id]['votes'][ctx.author.id] = winner_id
-            game = self.games[ctx.channel.id]
-            if game['voting_message'] is None:
-                self.games[ctx.channel.id]['voting_message'] = await ctx.channel.send(make_voting_message(game))
-            else:
-                await self.games[ctx.channel.id]['voting_message'].edit(content=make_voting_message(game))
-            if all(vote == game['votes'][ctx.author.id] for vote in game['votes'].values()):
-                await self.round_winner_set(ctx=ctx, winner=winner)
+                "I'm sorry, but this is not a known channel.\
+                 Please retry your command in a channel for a tournament match.")
         else:
-            await ctx.channel.send('''I'm sorry, I don't know who you're talking about! Please use the command as follows, mentioning the person who won:
-~winner <@689549152275005513>''')
-
-    @tournament.command(name="delete")
-    @commands.has_any_role('Tournament Master')
-    async def tourney_delete(self, ctx: commands.context.Context):
-        """Deletes the specified tournament."""
-        await ctx.message.delete()
-        await (await ctx.send('Ok, I deleted the tournament')).delete(delay=5)
-        for game in self.games:
-            try:
-                await self.games[game]['tc'].delete()
-            except:
-                pass
-            try:
-                await self.games[game]['vc'].delete()
-            except:
-                pass
-
-    @tournament.command(name="broadcast")
-    @commands.has_any_role('Tournament Master')
-    async def tourney_broadcast(self, ctx: commands.context.Context, message):
-        """Sends a message to all in-progress games."""
-        for game in self.games:
-            try:
-                await self.games[game]['tc'].send(message)
-            except:
-                print('Error sending broadcast')
+            await ctx.channel.send('''I'm sorry, I don't know who you're talking about! Please use the command like so,\
+             mentioning the person who won:
+            ~tournament winner <@689549152275005513>''')
+            return
+    #
+    # @tournament.command(name="delete")
+    # @commands.has_any_role('Tournament Master')
+    # async def tourney_delete(self, ctx: commands.context.Context):
+    #     """Deletes the specified tournament."""
+    #     await ctx.message.delete()
+    #     await (await ctx.send('Ok, I deleted the tournament')).delete(delay=5)
+    #     for game in self.games:
+    #         try:
+    #             await self.games[game]['tc'].delete()
+    #         except:
+    #             pass
+    #         try:
+    #             await self.games[game]['vc'].delete()
+    #         except:
+    #             pass
+    #
+    # @tournament.command(name="broadcast")
+    # @commands.has_any_role('Tournament Master')
+    # async def tourney_broadcast(self, ctx: commands.context.Context, message):
+    #     """Sends a message to all in-progress games."""
+    #     for game in self.games:
+    #         try:
+    #             await self.games[game]['tc'].send(message)
+    #         except:
+    #             print('Error sending broadcast')
 
 
 def setup(bot):
     bot.add_cog(TournamentCog(bot))
-
-
-def make_join_message(game, emoji, gamers):
-    msg = f"{game} tournament: react with {emoji} to join!"
-    if len(gamers) > 0:
-        msg += f"Currently participating - {len(gamers)}:\n"
-        if len(gamers) < 50:
-            for gamer in gamers:
-                msg += f"<@{gamer}\n"
-    return msg[:1999]
-
-
-def make_running_message(game, games, round):
-    msg = f'''{game} Tournament:
-Current matches (Round {round}):
-'''
-    for g in games:
-        out = f'''Match {games[g]['idx']} - '''
-        if games[g]['winner']:
-            out += f'''Winner: <@{games[g]['winner']}>
-'''
-        else:
-            out += '''In progress
-'''
-        msg += out
-    return msg[:1999]
-
-
-async def create_games(groups, ctx, game_name='Gaming', overwrites=None, category=None):
-    games = {}
-    for idx, group in enumerate(groups):
-        tc = await ctx.guild.create_text_channel(name=f"game-{idx}📋",
-                                                 overwrites=overwrites,
-                                                 category=ctx.guild.get_channel(category),
-                                                 topic=f"A channel for the {game_name} tournament!")
-        vc = await ctx.guild.create_voice_channel(name=f"game-{idx}🔊",
-                                                  overwrites=overwrites,
-                                                  category=ctx.guild.get_channel(category),
-                                                  topic=f"A channel for the {game_name} tournament!")
-        games[tc.id] = {
-            'gamers': group,
-            'tc': tc,
-            'vc': vc,
-            'winner': None,
-            'idx': idx,
-            'voting_message': None,
-            'votes': {gamer: None for gamer in group}
-        }
-    return games
-
-def make_voting_message(game):
-    out = f'''Winner for game {game['idx']}:
-'''
-    for gamer in game['gamers']:
-        out += f'''
-<@{gamer}> - '''
-        if game['votes'][gamer] is None:
-            out += 'Not yet voted'
-        else:
-            out += f'<@{game["votes"][gamer]}>'
-
-    return out[:1999]
