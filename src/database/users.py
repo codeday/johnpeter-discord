26099deb6d58e26a0748764b810ffe75dc4ff96f
Zeke Egherman <z@srnd.org>
2020-04-04 19:18:46 -0500

    make clear_username required

--- src/database/users.py
+++ src/database/users.py
@@ -1,4 +1,4 @@
 class User:
-    def __init__(self, discord_id: int, clear_username: str = None):
+    def __init__(self, discord_id: int, clear_username: str):
         self.discord_id = discord_id
         self.clear_username = clear_username
