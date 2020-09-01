97a218475285bfd19a0fcb2a3b230b0b4c48deac
Lola <lola@egherman.com>
2020-09-01 14:28:55 -0500

    use contains lol final change to search alg

--- src/services/teamservice.py
+++ src/services/teamservice.py
@@ -42,7 +42,7 @@ class TeamService:
         if session is None:
             session = session_creator()
             sess_flag = True
-        teams = session.query(Team).filter(Team.team_name.ilike(name)).all()
+        teams = session.query(Team).filter(Team.team_name.contains(name)).all()
         if sess_flag:
             session.commit()
             session.close()
