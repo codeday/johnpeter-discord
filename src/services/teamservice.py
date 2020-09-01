ea5a02c5bb38996e18b8fa43ec113bffef0b33ca
Lola <lola@egherman.com>
2020-09-01 14:16:49 -0500

    cast team name to string

--- src/services/teamservice.py
+++ src/services/teamservice.py
@@ -42,7 +42,7 @@ class TeamService:
         if session is None:
             session = session_creator()
             sess_flag = True
-        teams = session.query(Team).filter(name in Team.team_name)
+        teams = session.query(Team).filter(name in str(Team.team_name))
         if sess_flag:
             session.commit()
             session.close()
