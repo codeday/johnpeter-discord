f014ca4d28068932adedf0c68994c77b771665c4
Jake <royalcows9@gmail.com>
2020-09-18 15:46:45 -0700

    Re-write reaction roles to be persistent
    Updated `on_raw_reaction_add` and `reaction_groups` to get it's data from the database, not a local variable. Also added `update_reaction_roles` which will be sure that all users who are reacted to the message have their roles. Useful for re-synchronizing the roles if something gets messed up. Not ran automatically yet, but should probably hook into startup somewhere. 
    
    Still need to write a clear command that wipes the database association and possibly removes the roles.

--- src/db/models.py
+++ src/db/models.py
@@ -31,11 +31,11 @@ class Team(Base):
     members = relationship("Members", back_populates="team", cascade="all, delete")
 
     def __str__(self):
-        return f'''---
+        return f"""---
     {self.team_name} ({len(self.members)} members)
     Project: {self.project}
     Channel: <#{self.tc_id}>
-    Members: {', '.join(f"<@{m.member_id}>" for m in self.members)}'''
+    Members: {', '.join(f"<@{m.member_id}>" for m in self.members)}"""
 
 
 class Members(Base):
@@ -47,8 +47,54 @@ class Members(Base):
     team = relationship("Team", back_populates="members", cascade="all, delete")
 
 
+class Reactions(Base):
+    """ Stores a role id, and a corresponding message ID that it should look for.
+    Allows for searching by a message ID and finding all corresponding role IDs
+    """
+
+    __tablename__ = "reactions"
+
+    id = Column(Integer, primary_key=True)
+    # TODO: These columns really should have been left as ints…
+    role_id = Column(BigInteger, nullable=False)
+    message_id = Column(BigInteger, nullable=False)
+    channel_id = Column(BigInteger, nullable=False)
+    # Currently, 0 for all, represents the reaction mode. 0 is the random selection mode, this is here in case we
+    # want to do multiple modes.
+    reaction_type = Column(BigInteger, nullable=False, default=0)
+
+    @staticmethod
+    def groupmsgs():
+        """
+        groupmsgs = {
+            "<message_id>": {
+                "channel_id": "<channel_id>",
+                "role_ids": ["<role_id>", "<role_id>"]
+            }
+        }
+        """
+        session = session_creator()
+        groupmsgs_db = session.query(Reactions).filter(Reactions.reaction_type == 0).all()
+        groupmsgs = {}
+
+        msg: Reactions
+        for msg in groupmsgs_db:
+            # We're not sure if message IDs are actually unique or not, this assumes they are, but even still it's
+            # not likely there will be a conflict, so we're probably fine...
+            if groupmsgs.get(msg.message_id):
+                groupmsgs[msg.message_id]["role_ids"] = [*groupmsgs[msg.message_id]["role_ids"], msg.role_id]
+            else:
+                groupmsgs[msg.message_id] = {}
+                groupmsgs[msg.message_id]["channel_id"] = msg.channel_id
+                groupmsgs[msg.message_id]["role_ids"] = [msg.role_id]
+        print(groupmsgs)
+        session.close()
+        return groupmsgs
+
+
 def session_creator() -> Session:
     session = sessionmaker(bind=engine)
     return session()
+
 
 global_session: Session = session_creator()
