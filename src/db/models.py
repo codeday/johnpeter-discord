1ca278a821bdc8cf5eb64387dbab7fb3123d64f2
Lola <lola@egherman.com>
2020-10-15 14:17:40 -0500

    add creation timestamp (for grafana)

--- src/db/models.py
+++ src/db/models.py
@@ -29,6 +29,7 @@ class Team(Base):
     join_message_id = Column(String, nullable=False)
     project = Column(String)
     members = relationship("Members", back_populates="team", cascade="all, delete")
+    time = Column(DateTime, server_default=text('now()'))
 
     def __str__(self):
         return f"""---
@@ -45,6 +46,7 @@ class Members(Base):
     team_id = Column(Integer, ForeignKey("team.id"))
     member_id = Column(String, nullable=False)
     team = relationship("Team", back_populates="members", cascade="all, delete")
+    time = Column(DateTime, server_default=text('now()'))
 
 
 class Reactions(Base):
