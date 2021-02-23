5f7fbdd800b860d5bf15ac9a44378e8587eb8a49
Lynne Cure <pjh.cure@gmail.com>
2021-02-22 22:54:21 -0500

    Make john reply to messages rather than speak into the unfeeling void

--- src/cogs/cleverbot.py
+++ src/cogs/cleverbot.py
@@ -39,7 +39,7 @@ class CleverbotCog(commands.Cog, name="Cleverbot"):
             )
             msg_out = json.loads(r.text)["output"]
             self.states[state_id] = json.loads(r.text)["cs"]
-            await ctx.send(content=str(msg_out))
+            await ctx.reply(content=str(msg_out))
         if (
             isinstance(ctx.channel, discord.channel.DMChannel)
             and ctx.author is not ctx.channel.me
